59eebdd8a90b50eae4416f56e2224aaeacb4f540
Songeun <hannahkeeien@gmail.com>
2024-05-23 13:17:27 +0900

    refactor: productInfo 요소 이름 수정

--- src/components/common/Card/index.tsx
+++ src/components/common/Card/index.tsx
@@ -1,33 +1,37 @@
 import Image from 'next/image';
 import Link from 'next/link';
 
-type CardInfo = {
+import styles from './Card.module.scss';
+
+type ProductInfo = {
   title: string;
-  imageUrl: string;
-  price: number;
+  thumbNailImage: string;
+  originalPrice: number;
   discountRate: number;
-  discountedPrice: number;
+  price: number;
   starRating: number;
-  tags: string[];
+  reviewCount: number;
+  stock: number;
 };
 
 type CardProps = {
-  cardInfo: CardInfo;
+  productInfo: ProductInfo;
 };
 
-export default function Card({ cardInfo }: CardProps) {
-  const { title, imageUrl, price, discountRate, discountedPrice, starRating, tags } = cardInfo;
+export default function Card({ productInfo }: CardProps) {
+  const { title, thumbNailImage, originalPrice, discountRate, price, starRating, reviewCount, stock } = productInfo;
+
   return (
     <Link href={`/product/${title}`}>
-      <Image src={imageUrl} alt={title} width={500} height={300} />
+      <Image src={thumbNailImage} alt={title} width={500} height={300} />
       <div className="card-content">
         <h3>{title}</h3>
-        <p>{price}</p>
+        <p>{originalPrice}</p>
         <p>{discountRate}%</p>
-        <p>{discountedPrice}</p>
-        <Image src={'별 이미지'} alt="별" />
+        <p>{price}</p>
+        <Image src={'/path/to/star-image.png'} alt="별" />
         <p>{starRating}</p>
-        <div className="tags">
+        <div className={styles.tags}>
           {tags.map((tag, index) => (
             //태그 컴포넌트 만들기
             <span key={index} className="tag">
