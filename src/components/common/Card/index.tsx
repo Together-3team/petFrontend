f5f4bb66c0155d571eb67c96f3274543099ca21b
Songeun <hannahkeeien@gmail.com>
2024-05-23 16:28:35 +0900

    refactor: 상품 태그 적용

--- src/components/common/Card/index.tsx
+++ src/components/common/Card/index.tsx
@@ -1,7 +1,9 @@
 import Image from 'next/image';
 import Link from 'next/link';
+import classNames from 'classnames/bind';
 
 import styles from './Card.module.scss';
+import Tag from '../Tag';
 
 type ProductInfo = {
   title: string;
@@ -16,14 +18,18 @@ type ProductInfo = {
 
 type CardProps = {
   productInfo: ProductInfo;
+  wishList: boolean;
 };
 
-export default function Card({ productInfo }: CardProps) {
+export default function Card({ productInfo, wishList }: CardProps) {
   const { title, thumbNailImage, originalPrice, discountRate, price, starRating, reviewCount, stock } = productInfo;
 
   return (
     <Link href={`/product/${title}`}>
       <Image src={thumbNailImage} alt={title} width={500} height={300} />
+      <button>
+        <Image src={'heart'} alt="찜하기" />
+      </button>
       <div className="card-content">
         <h3>{title}</h3>
         <p>{originalPrice}</p>
@@ -32,12 +38,8 @@ export default function Card({ productInfo }: CardProps) {
         <Image src={'/path/to/star-image.png'} alt="별" />
         <p>{starRating}</p>
         <div className={styles.tags}>
-          {tags.map((tag, index) => (
-            //태그 컴포넌트 만들기
-            <span key={index} className="tag">
-              {tag}
-            </span>
-          ))}
+          {stock <= 10 && <Tag>재고 10개 미만</Tag>}
+          {reviewCount >= 100 && <Tag>리뷰 100개 이상</Tag>}
         </div>
       </div>
     </Link>
