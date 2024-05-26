64050dd2af4a95bf7fe2104b070e025c15731d39
devwqc <devwqc@gmail.com>
2024-05-27 01:16:00 +0900

    feat: HomePage 레이아웃 테스트 작업

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,15 @@
+import classNames from 'classnames/bind';
+
 import MainLayout from '@/components/common/Layout/Main';
-import styles from './Home.module.scss';
+import styles from './HomePage.module.scss';
+
+const cx = classNames.bind(styles);
 
 export default function HomePage() {
   return (
     <>
-      <h1 className={styles.home}>HomePage</h1>
+      <h1 className={cx('home')}>HomePage</h1>
+      <div className={cx('test')}></div>
     </>
   );
 }
