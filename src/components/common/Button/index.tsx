2b82940d6d7b775dbb21e1bae2f49b7f7a2fbe20
seolsis <seoljw1005@naver.com>
2024-05-28 23:50:10 +0900

    fix: button 색상 수정

--- src/components/common/Button/index.tsx
+++ src/components/common/Button/index.tsx
@@ -3,13 +3,19 @@ import styles from './Button.module.scss';
 interface ButtonProps {
   size: 'large' | 'medium';
   children?: React.ReactNode;
-  backgroundColor: '#545454' | '#FFFFFF';
+  backgroundColor: '#373C49' | '#cfd3db' | '#fe5a65' | '#ffffff';
   onClick?: () => void;
 }
 
 export default function Button({ size, children, backgroundColor, onClick }: ButtonProps) {
   const sizeClass = styles[size];
-  const backgroundClass = backgroundColor === '#545454' ? styles.backgroundBlack : styles.backgroundWhite;
+  const backgroundColorMap: { [key: string]: string } = {
+    '#373C49': styles.backgroundBlack,
+    '#cfd3db': styles.backgroundGray,
+    '#fe5a65': styles.backgroundPink,
+    '#ffffff': styles.backgroundWhite,
+  };
+  const backgroundClass = backgroundColorMap[backgroundColor] || '';
   const className = `${styles.button} ${sizeClass} ${backgroundClass}`;
 
   return (
