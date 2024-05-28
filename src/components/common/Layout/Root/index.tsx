23285080f428571a9b83ce5b571a0512ff28d183
devwqc <devwqc@gmail.com>
2024-05-28 15:14:26 +0900

    design: RootLayout 레이아웃 스타일링

--- src/components/common/Layout/Root/index.tsx
+++ src/components/common/Layout/Root/index.tsx
@@ -2,15 +2,19 @@ import { PropsWithChildren } from 'react';
 import classNames from 'classnames/bind';
 
 import styles from './RootLayout.module.scss';
+import MainFooter from '../Main/Footer';
 
 const cx = classNames.bind(styles);
 
 export default function RootLayout({ children }: PropsWithChildren) {
   return (
-    <>
+    <div className={cx('container')}>
       <div className={cx('background')} />
-      <div className={cx('hero')}></div>
-      <div className={cx('contents')}>{children}</div>
-    </>
+      <div className={cx('home')}></div>
+      <div className={cx('contents')}>
+        <div className={cx('main')}>{children}</div>
+        <MainFooter />
+      </div>
+    </div>
   );
 }
