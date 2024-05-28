f23ed3fa1609647470ff6a950232bf868adf2d0b
Seoin <144193370+Seoin02@users.noreply.github.com>
2024-05-28 17:05:56 +0900

    feat: 카카오 로그인 요청 구현

--- src/constants/oAuth.ts
+++ src/constants/oAuth.ts
@@ -1,4 +1,4 @@
-export const CLIENT_ID = import.meta.env.REST_API_KEY;
-export const REDIRECT_URL = import.meta.env.REDIRECT_URL;
+export const CLIENT_ID = process.env.NEXT_PUBLIC_REST_API_KEY;
+export const REDIRECT_URL = process.env.NEXT_PUBLIC_REDIRECT_URL;
 
 export const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URL}&response_type=code&prompt=login`;
