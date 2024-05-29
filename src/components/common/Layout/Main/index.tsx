8d6270aaca840f86e06fa11204e8671798701991
devwqc <devwqc@gmail.com>
2024-05-30 00:29:15 +0900

    feat: MainLayout relativeBox 구현 후 top 버튼 적용

--- src/components/common/Layout/Main/index.tsx
+++ src/components/common/Layout/Main/index.tsx
@@ -1,17 +1,17 @@
 import { PropsWithChildren } from 'react';
-import classNames from 'classnames/bind';
 
 import styles from './MainLayout.module.scss';
 import MainHeader from './Header';
 import MainFooter from './Footer';
 
-const cx = classNames.bind(styles);
-
 export default function MainLayout({ children }: PropsWithChildren) {
   return (
-    <div className={cx('contents')}>
+    <div className={styles.contents}>
       <MainHeader />
-      {children}
+      <main className={styles.main}>{children}</main>
+      <div className={styles.relativeBox}>
+        <button className={styles.topButton}>top</button>
+      </div>
       <MainFooter />
     </div>
   );
