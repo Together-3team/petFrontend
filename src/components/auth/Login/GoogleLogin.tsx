f0e9239cc86fd19d92381630213f3bb40fb4441a
Seoin <144193370+Seoin02@users.noreply.github.com>
2024-05-29 15:18:25 +0900

    design: kakao-yellow 컬러 변수 추가

--- src/components/auth/Login/GoogleLogin.tsx
+++ src/components/auth/Login/GoogleLogin.tsx
@@ -1,12 +1,17 @@
 import { GOOGLE_AUTH_URL } from '@/constants/oAuth';
 import Link from 'next/link';
+import classNames from 'classnames/bind';
+import GoogleLogo from '@/assets/svgs/google-logo.svg';
+import styles from './Login.module.scss';
+
+const cx = classNames.bind(styles);
 
 export default function GoogleLogin() {
   return (
-    <>
-      <Link href={GOOGLE_AUTH_URL}>
+    <Link href={GOOGLE_AUTH_URL}>
+      <div className={cx('googleButton')}>
         <div>Google로 계속하기</div>
-      </Link>
-    </>
+      </div>
+    </Link>
   );
 }
