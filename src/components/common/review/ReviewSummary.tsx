89a92627130d63a2a4d2de678b8fd6cf54e40424
minyoung <alsdyd1021@naver.com>
2024-05-31 03:19:06 +0900

    feat: reviewSummary UI setting

--- src/components/common/review/ReviewSummary.tsx
+++ src/components/common/review/ReviewSummary.tsx
@@ -1,3 +1,24 @@
+import StarRating from './StarRating';
+import styles from './ReviewSummary.module.scss';
+
 export default function ReviewSummary() {
-  return <div>ReviewSummary</div>;
+  const rating = 4.5;
+  const totalReviewer = 180;
+
+  return (
+    <div>
+      <div>
+        <p>리뷰</p>
+        <p>리뷰 개수</p>
+      </div>
+      <div>
+        <StarRating rating={rating} />
+        <p>
+          {rating}
+          <span>/ 5.0</span>
+        </p>
+        <p>{`(${totalReviewer}명)`}</p>
+      </div>
+    </div>
+  );
 }
