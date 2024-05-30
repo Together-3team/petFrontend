4fed6dbc52493182ef59090e56253e05ef4b7bdd
Seoin <144193370+Seoin02@users.noreply.github.com>
2024-05-30 22:36:14 +0900

    feat: 이용약관 연결

--- src/components/auth/SignupForm/UserAgreement.tsx
+++ src/components/auth/SignupForm/UserAgreement.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link';
 import classNames from 'classnames/bind';
 import styles from './UserAgreement.module.scss';
 
@@ -16,17 +17,29 @@ export default function UserAgreement() {
           <div className={cx('agreement')}>
             <input type="checkbox" className={cx('check')} />
             <span className={cx('inputCenter')}>(필수) 서비스 이용약관 동의</span>
-            <div className={cx('detail')}>자세히</div>
+            <Link
+              className={cx('detail')}
+              href="https://vast-nephew-587.notion.site/554194d084c64caaba2c165f4b803708?pvs=4">
+              자세히
+            </Link>
           </div>
           <div className={cx('agreement')}>
             <input type="checkbox" className={cx('check')} />
             <span className={cx('inputCenter')}>(필수) 개인정보 수집 및 이용 동의</span>
-            <div className={cx('detail')}>자세히</div>
+            <Link
+              className={cx('detail')}
+              href="https://vast-nephew-587.notion.site/367187c8edb6468d8cbc197c688cb4eb?pvs=4">
+              자세히
+            </Link>
           </div>
           <div className={cx('agreement')}>
             <input type="checkbox" className={cx('check')} />
             <span className={cx('inputCenter')}>(선택) 광고성 정보 수신 전체 동의</span>
-            <div className={cx('detail')}>자세히</div>
+            <Link
+              className={cx('detail')}
+              href="https://vast-nephew-587.notion.site/25d52ac8eaac43df93fdedfbeeec442f?pvs=4">
+              자세히
+            </Link>
           </div>
         </div>
       </div>
