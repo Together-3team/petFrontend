a9425564f19f875e72dae4b39c16ef09f7d4fdeb
seolsis <seoljw1005@naver.com>
2024-05-31 16:08:43 +0900

    feat: 토스결제 api 사용

--- src/pages/Payment/index.tsx
+++ src/pages/Payment/index.tsx
@@ -1,16 +1,60 @@
-import { useState } from 'react';
 import styles from './Payment.module.scss';
+import { useEffect } from 'react';
 
-export default function Payment() {
+interface PaymentProps {
+  totalPrice: number;
+  totalOriginalPrice: number;
+  productCount: number;
+}
+
+declare global {
+  interface Window {
+    TossPayments: any;
+  }
+}
+
+export default function Payment({ productCount }: PaymentProps) {
+  useEffect(() => {
+    const script = document.createElement('script');
+    script.src = 'https://js.tosspayments.com/v1';
+    script.async = true;
+    document.body.appendChild(script);
+    return () => {
+      document.body.removeChild(script);
+    };
+  }, []);
+
+  const totalPrice = 10;
+  const handlePayment = async () => {
+    if (!window.TossPayments) {
+      console.error('Toss Payments script not loaded');
+      return;
+    }
+
+    const tossPayments = window.TossPayments(process.env.NEXT_PUBLIC_TOSS_PAYMENTS_CLIENT_KEY);
+
+    tossPayments.requestPayment('카드', {
+      amount: totalPrice,
+      orderId: 'orderId-12345',
+      orderName: 'Test Order',
+      customerName: 'Customer',
+      successUrl: `${window.location.origin}/payment/success`,
+      failUrl: `${window.location.origin}/payment/fail`,
+    });
+  };
   return (
     <>
       <div className={styles.payment}>
         <div className={styles.orderProduct}>
           <div className={styles.orderTitle}>
             <div>주문상품</div>
-            <div>n개</div>
+            <div>{productCount}개</div>
           </div>
         </div>
+        <div className={styles.paymentBank}>
+          <button onClick={handlePayment}>결제하기</button>
+        </div>
+        {/* <TotalPay totalPrice={totalPrice} totalOriginalPrice={totalOriginalPrice} productCount={productCount} /> */}
       </div>
     </>
   );
