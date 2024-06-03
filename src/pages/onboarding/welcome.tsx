283f53ef471cffcdc340e6215946d9cf688b25cc
Seoin <144193370+Seoin02@users.noreply.github.com>
2024-06-03 21:15:51 +0900

    feat: welcome 페이지 자동 전환 구현

--- src/pages/onboarding/welcome.tsx
+++ src/pages/onboarding/welcome.tsx
@@ -1,8 +1,13 @@
+import { useRouter } from 'next/router';
 import ImageBox from '@/components/common/ImageBox';
 import Image from '@/assets/exampleProductImg.jpg';
 import styles from './Onboarding.module.scss';
 
 export default function Welcome() {
+  const router = useRouter();
+  setTimeout(() => {
+    router.push('/');
+  }, 2000);
   return (
     <div className={styles.welcomePage}>
       <h1 className={styles.welcomeTitle}>환영합니다, 해피사랑님!</h1>
