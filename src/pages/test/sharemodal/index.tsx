e387484217af5f63a8e4e9acf82f22469172a9b7
Songeun <hannahkeeien@gmail.com>
2024-06-03 18:09:59 +0900

    chore: 불필요한 태그 삭제

--- src/pages/test/sharemodal/index.tsx
+++ src/pages/test/sharemodal/index.tsx
@@ -14,30 +14,28 @@ export default function ShareModalPage() {
   const { modalOpen, handleModalOpen, handleModalClose } = useModal();
 
   return (
-    <>
-      <div className={cx('my')}>
-        <Input id="이메일" type="email" label="이메일" size={'large'} background={'background'} placeholder=" " />
-        <Input id="이름" type="text" label="이름" size={'large'} background={'background'} placeholder=" " />
-        <div onClick={handleModalOpen}>모달 열기</div>
-        <BottomShareModal
-          type="share"
-          className={cx('')}
-          modalOpen={modalOpen}
-          handleModalOpen={handleModalOpen}
-          handleModalClose={handleModalClose}>
-          <Image
-            src={titleImage}
-            alt={'대표이미지'}
-            width={100}
-            height={100}
-            blurDataURL={'@/assets/svgs/rectangle.svg'}
-            placeholder="blur"
-          />
-          <div className={cx('title')}>
-            <span>집사 친구와 함께 더 저렴하게 구매하세요!</span>
-          </div>
-        </BottomShareModal>
-      </div>
-    </>
+    <div className={cx('my')}>
+      <Input id="이메일" type="email" label="이메일" size={'large'} background={'background'} placeholder=" " />
+      <Input id="이름" type="text" label="이름" size={'large'} background={'background'} placeholder=" " />
+      <div onClick={handleModalOpen}>모달 열기</div>
+      <BottomShareModal
+        type="share"
+        className={cx('')}
+        modalOpen={modalOpen}
+        handleModalOpen={handleModalOpen}
+        handleModalClose={handleModalClose}>
+        <Image
+          src={titleImage}
+          alt={'대표이미지'}
+          width={100}
+          height={100}
+          blurDataURL={'@/assets/svgs/rectangle.svg'}
+          placeholder="blur"
+        />
+        <div className={cx('title')}>
+          <span>집사 친구와 함께 더 저렴하게 구매하세요!</span>
+        </div>
+      </BottomShareModal>
+    </div>
   );
 }
