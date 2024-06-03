349355c0e7cc8b3b62c37c2d0a7836b2af32726a
Songeun <hannahkeeien@gmail.com>
2024-06-03 11:43:59 +0900

    feat: shareModal 사용 예시 추가

--- src/pages/test/sharemodal/index.tsx
+++ src/pages/test/sharemodal/index.tsx
@@ -1,8 +1,11 @@
+import Image from 'next/image';
 import classNames from 'classnames/bind';
+
 import useModal from '@/hooks/useModal';
 import Input from '@/components/common/Input';
 import BottomShareModal from '@/components/common/Modal/BottomShareModal';
 import Button from '@/components/common/Button';
+import titleImage from '@/assets/images/rectangle.png';
 import styles from './ShareModalPage.module.scss';
 
 const cx = classNames.bind(styles);
@@ -22,16 +25,16 @@ export default function ShareModalPage() {
           modalOpen={modalOpen}
           handleModalOpen={handleModalOpen}
           handleModalClose={handleModalClose}>
-          <div className={cx('modalSize')}>
-            <span>배송지를 삭제하시겠습니까?</span>
-            <div className={cx('buttonArea')}>
-              <Button size="medium" backgroundColor="$color-white" onClick={handleModalClose}>
-                취소
-              </Button>
-              <Button size="medium" backgroundColor="$color-gray-800">
-                삭제
-              </Button>
-            </div>
+          <Image
+            src={titleImage}
+            alt={'대표이미지'}
+            width={100}
+            height={100}
+            blurDataURL={'@/assets/svgs/rectangle.svg'}
+            placeholder="blur"
+          />
+          <div className={cx('title')}>
+            <span>집사 친구와 함께 더 저렴하게 구매하세요!</span>
           </div>
         </BottomShareModal>
       </div>
