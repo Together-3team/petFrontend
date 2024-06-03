e8978c09eb2bc2b3c7a9c3a8239d096b21a2bcac
Seoin <144193370+Seoin02@users.noreply.github.com>
2024-06-03 19:05:02 +0900

    fix: 머지 충돌 해결

--- src/components/common/Modal/BottomShareModal/ModalBase.tsx
+++ src/components/common/Modal/BottomShareModal/ModalBase.tsx
@@ -1,20 +1,25 @@
 import { PropsWithChildren } from 'react';
 import classNames from 'classnames/bind';
 import ModalPortal from '@/components/common/Modal/Portal';
-import styles from './BottomModal.module.scss';
+import styles from './BottomShareModal.module.scss';
 
 export interface ModalProps {
+  type: 'bottom' | 'share';
   className?: string;
   onClose?: () => void;
 }
 
 const cx = classNames.bind(styles);
 
-export default function ModalBase({ className, children }: PropsWithChildren<ModalProps>) {
+export default function ModalBase({ type, className, children }: PropsWithChildren<ModalProps>) {
   return (
     <ModalPortal>
-      <div className={cx('modalBase', className)}>
-        <div className={cx('modalContent')}>{children}</div>
+      <div className={cx('modalBackground')} data-type={type}>
+        <div className={cx('modalBase', className)} data-type={type}>
+          <div className={cx('modalContent')} data-type={type}>
+            {children}
+          </div>
+        </div>
       </div>
     </ModalPortal>
   );
