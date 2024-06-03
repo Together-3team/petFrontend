339eb140a490fa5d4f4197eb4b9853c399793b77
seolsis <seoljw1005@naver.com>
2024-06-03 16:16:22 +0900

    feat: 동의 체크박스 생성

--- src/pages/Payment/index.tsx
+++ src/pages/Payment/index.tsx
@@ -1,5 +1,6 @@
+import Button from '@/components/common/Button';
 import styles from './Payment.module.scss';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
 interface PaymentProps {
   totalPrice: number;
@@ -14,6 +15,18 @@ declare global {
 }
 
 export default function Payment({ productCount }: PaymentProps) {
+  const [checkboxChecked, setCheckboxChecked] = useState(false);
+  const [check1Checked, setCheck1Checked] = useState(false);
+  const [check2Checked, setCheck2Checked] = useState(false);
+
+  useEffect(() => {
+    if (check1Checked && check2Checked) {
+      setCheckboxChecked(true);
+    } else {
+      setCheckboxChecked(false);
+    }
+  }, [check1Checked, check2Checked]);
+
   useEffect(() => {
     const script = document.createElement('script');
     script.src = 'https://js.tosspayments.com/v1';
@@ -23,6 +36,23 @@ export default function Payment({ productCount }: PaymentProps) {
       document.body.removeChild(script);
     };
   }, []);
+
+  function handleCheckboxChange() {
+    const newChecked = !checkboxChecked;
+    setCheckboxChecked(newChecked);
+    setCheck1Checked(newChecked);
+    setCheck2Checked(newChecked);
+  }
+
+  function handleCheck1Change() {
+    setCheck1Checked(!check1Checked);
+    console.log('check1');
+  }
+
+  function handleCheck2Change() {
+    setCheck2Checked(!check2Checked);
+    console.log('check2');
+  }
 
   const totalPrice = 10;
   const handlePayment = async () => {
@@ -47,14 +77,37 @@ export default function Payment({ productCount }: PaymentProps) {
       <div className={styles.payment}>
         <div className={styles.orderProduct}>
           <div className={styles.orderTitle}>
-            <div>주문상품</div>
-            <div>{productCount}개</div>
+            <div>
+              주문상품<span>{productCount}개</span>
+            </div>
           </div>
         </div>
-        <div className={styles.paymentBank}>
-          <button onClick={handlePayment}>결제하기</button>
+        <div className={styles.paymentAgree}>
+          <div className={styles.agree}>
+            <div className={styles.checkboxTitle}>
+              <input
+                type="checkbox"
+                className={styles.checkbox}
+                checked={checkboxChecked}
+                onChange={handleCheckboxChange}
+              />
+              <div className={styles.agreeTitle}>주문내용 확인 및 결제 동의</div>
+            </div>
+            <div className={styles.contentCheckbox}>
+              <input type="checkbox" className={styles.check} checked={check1Checked} onChange={handleCheck1Change} />
+              <div className={styles.checkContent}>(필수) 개인정보 수집, 이용 동의</div>
+            </div>
+            <div className={styles.contentCheckbox}>
+              <input type="checkbox" className={styles.check} checked={check2Checked} onChange={handleCheck2Change} />
+              <div className={styles.checkContent}>(필수) 개인정보 제3자 정보 제공 동의</div>
+            </div>
+          </div>
+          <div className={styles.paymentButton}>
+            <Button size="large" backgroundColor="$color-pink-main" onClick={handlePayment}>
+              주문하기
+            </Button>
+          </div>
         </div>
-        {/* <TotalPay totalPrice={totalPrice} totalOriginalPrice={totalOriginalPrice} productCount={productCount} /> */}
       </div>
     </>
   );
