5d6765301ffe3b53610566a3bede52eac5e48db4
minyoung <alsdyd1021@naver.com>
2024-06-06 02:49:34 +0900

    design: highlightTeam 컴포넌트 teamBox 제외 css 적용

--- src/components/common/HighlightTeam/index.tsx
+++ src/components/common/HighlightTeam/index.tsx
@@ -1,4 +1,5 @@
 import Link from 'next/link';
+import styles from './HighlightTeam.module.scss';
 
 export default function HighlightTeam() {
   const testData = [
@@ -46,13 +47,15 @@ export default function HighlightTeam() {
   ];
 
   return (
-    <div>
-      <div>
-        <p>2인 공동구매</p>
-        <p>주문참여로 기다림 없이 바로 주문해보세요</p>
+    <section className={styles.highlightTeamLayout}>
+      <div className={styles.sectionTitleBox}>
+        <p className={styles.title}>2인 공동구매</p>
+        <p className={styles.description}>주문참여로 기다림 없이 바로 주문해보세요</p>
       </div>
-      <div>공동구매 data</div>
-      <Link href={'/test/team'}>참여자 전체보기</Link>
-    </div>
+      <div className={styles.teamBox}>공동구매 data</div>
+      <Link href={'/test/team'} className={styles.allTeamLinkBtn}>
+        참여자 전체보기
+      </Link>
+    </section>
   );
 }
