5eef865191b4fcb82d6f76e22cc979ee4b3bfdaf
Songeun <hannahkeeien@gmail.com>
2024-06-05 10:45:13 +0900

    feat: drawer 애니메이션 추가

--- src/components/common/Modal/AdaptiveModal/index.tsx
+++ src/components/common/Modal/AdaptiveModal/index.tsx
@@ -2,7 +2,9 @@ import classNames from 'classnames/bind';
 import { useModalProps } from '@/hooks/useModal';
 import ModalBase, { ModalProps } from '@/components/common/Modal/AdaptiveModal/ModalBase';
 import styles from './AdaptiveModal.module.scss';
-import { PropsWithChildren } from 'react';
+import { PropsWithChildren, useRef } from 'react';
+import { useSpring, animated } from 'react-spring';
+import { useDrag } from 'react-use-gesture';
 
 const cx = classNames.bind(styles);
 
@@ -14,17 +16,47 @@ export default function AdaptiveModal({
   className,
   children,
 }: PropsWithChildren<useModalProps & ModalProps>) {
+  const modalRef = useRef(null);
+  const [{ y }, api] = useSpring(() => ({ y: 0 }));
+
+  const openModal = () => {
+    api.start({ y: 0 });
+    handleModalOpen();
+  };
+
+  const closeModal = () => {
+    api.start({ y: window.innerHeight });
+    setTimeout(handleModalClose, 300);
+  };
+
+  const bind = useDrag(
+    ({ down, movement: [, my], direction: [, dy], velocity }) => {
+      if (down && dy > 0 && my > 100) {
+        closeModal();
+      } else {
+        api.start({ y: down ? my : 0 });
+      }
+    },
+    { initial: () => [0, y.get()], filterTaps: true, bounds: { top: 0 }, rubberband: true }
+  );
+
   return (
     <div
       className={cx('modalWrapper')}
       onClick={e => {
         if (e.target === e.currentTarget) {
-          handleModalClose();
+          type === 'drawer' ? closeModal() : handleModalClose();
         }
       }}>
       {modalOpen && (
-        <ModalBase type={type} className={className} onClose={handleModalClose}>
-          {children}
+        <ModalBase type={type} className={className} onClose={type === 'drawer' ? closeModal : handleModalClose}>
+          <animated.div
+            ref={modalRef}
+            {...bind()}
+            style={{ transform: y.to(y => `translateY(${y}px)`) }}
+            className={cx('modalContent', { [type]: type })}>
+            {children}
+          </animated.div>
         </ModalBase>
       )}
     </div>
