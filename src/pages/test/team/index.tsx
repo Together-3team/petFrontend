b19a6b17bc0e0b24497fe1fd60c6ace9b07d1e9c
minyoung <alsdyd1021@naver.com>
2024-06-08 02:24:52 +0900

    feat: TeamPage Header 적용

--- src/pages/test/team/index.tsx
+++ src/pages/test/team/index.tsx
@@ -1,9 +1,23 @@
+import BackButton from '@/components/common/BackButton';
 import HighlightTeam from '@/components/common/HighlightTeam';
+import Header from '@/components/common/Layout/Header';
+import styles from './TeamPage.module.scss';
 
 export default function TeamPage() {
   return (
     <div>
-      <HighlightTeam />
+      <Header.Root>
+        <Header.Box>
+          <Header.Left>
+            <BackButton />
+          </Header.Left>
+          <Header.Center>페이지 전체보기</Header.Center>
+        </Header.Box>
+      </Header.Root>
+
+      <div>
+        <HighlightTeam />
+      </div>
     </div>
   );
 }
