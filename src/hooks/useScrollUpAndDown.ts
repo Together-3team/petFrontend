3d6f511d40e25a5a39da1969b17f0297b7952cc0
devwqc <devwqc@gmail.com>
2024-06-07 11:38:32 +0900

    comment: useScrollUpAndDown 사용법 작성

--- src/hooks/useScrollUpAndDown.ts
+++ src/hooks/useScrollUpAndDown.ts
@@ -1,5 +1,18 @@
 import { useEffect, useState } from 'react';
 
+/*
+  상하 스크롤을 감지하는 훅입니다.
+  minScrollY로 최소 스크롤 위치를 지정할 수 있습니다.
+
+  사용)
+  export default function Example() {
+    const { isUp } = useScrollUpAndDown();
+
+    return (
+      <div style={{ display: isUp ? 'block' : 'none' }}>보이나요?</div>
+    );
+  }
+*/
 export default function useScrollUpAndDown(minScrollY: number = 0) {
   const [isUp, setIsUp] = useState(true);
 
