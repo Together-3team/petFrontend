e49435749087ca4262ea9628905ba665d488cc7d
devwqc <devwqc@gmail.com>
2024-06-07 10:39:18 +0900

    feat: Header에 useScrollVisible 적용

--- src/components/common/Layout/Header/Root/index.tsx
+++ src/components/common/Layout/Header/Root/index.tsx
@@ -2,12 +2,15 @@ import { ComponentPropsWithoutRef } from 'react';
 import classNames from 'classnames/bind';
 
 import styles from './HeaderRoot.module.scss';
+import useScrollVisible from '@/hooks/useScrollVisible';
 
 const cx = classNames.bind(styles);
 
 export default function HeaderRoot({ children, className, ...rest }: ComponentPropsWithoutRef<'header'>) {
+  const { visible } = useScrollVisible();
+
   return (
-    <header className={cx('header', className)} {...rest}>
+    <header className={cx('header', className)} data-visible={visible ? 'show' : 'hidden'} {...rest}>
       {children}
     </header>
   );
