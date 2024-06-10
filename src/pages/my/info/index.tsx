9f875970efc6c661d2e1b4d5951f3fb2593b1a02
Seoin <144193370+Seoin02@users.noreply.github.com>
2024-06-10 14:32:27 +0900

    feat: my/info 페이지 UI 구현

--- src/pages/my/info/index.tsx
+++ src/pages/my/info/index.tsx
@@ -1,3 +1,4 @@
+import { useRef } from 'react';
 import useModal from '@/hooks/useModal';
 import Header from '@/components/common/Layout/Header';
 import Input from '@/components/common/Input';
@@ -8,12 +9,15 @@ import Sample from '@/assets/exampleProductImg.jpg';
 
 import styles from './Info.module.scss';
 import ImageBox from '@/components/common/ImageBox';
+import useOutsideClick from '@/hooks/useOutsideClick';
 
 export default function Info() {
   const { modalOpen, handleModalOpen, handleModalClose } = useModal();
+  const containerRef = useRef<HTMLDivElement>(null);
+  useOutsideClick(containerRef, () => handleModalClose());
 
   return (
-    <div className={styles.infoLayout}>
+    <div className={styles.infoLayout} ref={containerRef}>
       <Header.Root>
         <Header.Box>
           <Header.Left>
@@ -49,25 +53,30 @@ export default function Info() {
         <div className={styles.quitText} onClick={handleModalOpen}>
           회원탈퇴
         </div>
-        <BottomShareModal
-          type="bottom"
-          className=""
-          modalOpen={modalOpen}
-          handleModalOpen={handleModalOpen}
-          handleModalClose={handleModalClose}>
-          <div className={styles.modalSize}>
-            <span className={styles.modalTitle}>정말로 탈퇴하시겠어요?</span>
-            <ImageBox size="petPhoto" src={Sample} alt="강아지와 고양이가 울먹거리는 이미지" />
-            <div className={styles.modalButtonArea}>
-              <Button size="medium" backgroundColor="$color-white">
-                탈퇴하기
-              </Button>
-              <Button size="medium" backgroundColor="$color-gray-800" onClick={handleModalClose}>
-                돌아가기
-              </Button>
+        {handleModalOpen && (
+          <BottomShareModal
+            type="bottom"
+            modalOpen={modalOpen}
+            handleModalOpen={handleModalOpen}
+            handleModalClose={handleModalClose}>
+            <div className={styles.modalSize}>
+              <span className={styles.modalTitle}>정말로 탈퇴하시겠어요?</span>
+              <ImageBox size="modalImage" src={Sample} alt="강아지와 고양이가 울먹거리는 이미지" />
+              <span className={styles.modalText}>
+                • 찜한 상품 6개와 장바구니에 담은 상품 2개가 모두 사라져요
+                <br />• 우리 아이와의 추억이 담긴 리뷰 3개가 모두 사라져요
+              </span>
+              <div className={styles.modalButtonArea}>
+                <Button size="medium" backgroundColor="$color-white">
+                  탈퇴하기
+                </Button>
+                <Button size="medium" backgroundColor="$color-gray-800" onClick={handleModalClose}>
+                  돌아가기
+                </Button>
+              </div>
             </div>
-          </div>
-        </BottomShareModal>
+          </BottomShareModal>
+        )}
       </div>
     </div>
   );
