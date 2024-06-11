658666cfb474a529d0c925dd50e2c6e172ee0f35
Songeun <hannahkeeien@gmail.com>
2024-06-11 20:53:17 +0900

    feat: productInfo 정보 추가

--- src/components/productDetails/productInfo/index.tsx
+++ src/components/productDetails/productInfo/index.tsx
@@ -6,6 +6,7 @@ import StarRating from '@/components/common/review/StarRating';
 import Tag from '@/components/common/Tag';
 import BannerCarousel from '@/components/common/Carousel/Banner';
 import ProductCarousel from '@/components/common/Carousel/Product';
+import Delivery from '@/assets/svgs/delivery.svg';
 
 const cx = classNames.bind(styles);
 
@@ -45,19 +46,25 @@ export default function ProductInfo({
         /> */}
         <ProductCarousel items={productImages} />
       </div>
-      <h1 className={cx('title')}>{title}</h1>
-      <p>{originalPrice}</p>
-      <span>{discountRate}</span>
-      <span>{price}</span>
-      <StarRating rating={reviewRating} />
-      리뷰 {reviewCount}개
-      <div>
-        <Tag size="huge" type="stock" color="#FFF3F3" fontColor="#FE5A65">
-          10개 미만
-        </Tag>
-        <Tag size="huge" type="thumbsUp" color="#E5FAFC" fontColor="#34BACC">
-          리뷰 100+
-        </Tag>
+      <div className={cx('productInfoContent')}>
+        <h1 className={cx('title')}>{title}</h1>
+        <p>{originalPrice}</p>
+        <span>{discountRate}</span>
+        <span>{price}</span>
+        <StarRating rating={reviewRating} />
+        리뷰 {reviewCount}개
+        <div className={cx('tags')}>
+          <Tag size="huge" type="stock" color="#FFF3F3" fontColor="#FE5A65">
+            10개 미만
+          </Tag>
+          <Tag size="huge" type="thumbsUp" color="#E5FAFC" fontColor="#34BACC">
+            리뷰 100+
+          </Tag>
+        </div>
+        <div className={cx('shippingInfo')}>
+          <Delivery width={24} height={24} />
+          지금 주문하면 전 도착 예정{' '}
+        </div>
       </div>
     </>
   );
