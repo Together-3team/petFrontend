1ceac84b07b472a0707fb3b4bd2441ca47694514
Seoin <144193370+Seoin02@users.noreply.github.com>
2024-06-11 17:28:35 +0900

    feat: 구글 로그인 로직 구현

--- src/apis/authAPI.ts
+++ src/apis/authAPI.ts
@@ -1,35 +1,45 @@
 import { useRouter } from 'next/router';
 import saveTokenToCookie from '@/utils/cookie';
 import axiosInstance from './axiosInstance';
+import { API_BASE_URL } from '@/constants';
 
 //인증 관련 API 요청
 
-export async function GetGoogleAuth() {
-  const router = useRouter();
-  try {
-    const response = await axiosInstance.get(`/auth/google`);
-    if (response.data.registered === true) {
-      const { accessToken } = response.data;
-      saveTokenToCookie(accessToken);
-      router.push('/onboarding');
-    } else {
-      router.push('/signup');
-    }
-  } catch (error) {
-    console.log('토큰을 확인할 수 없습니다.', error);
-  }
+export interface GoogleAuthResponse {
+  registered: boolean;
+  profileToken: string;
+  accessToken?: string;
+}
+
+export async function GetGoogleAuth(): Promise<GoogleAuthResponse> {
+  const response = await axiosInstance.get(`/auth/google`);
+  return response.data;
+  // const router = useRouter();
+  // try {
+  //   const response = await axiosInstance.get(`/auth/google`);
+  //   console.log(response.data);
+  //   if (response.data.registered === true) {
+  //     const { accessToken } = response.data;
+  //     saveTokenToCookie(accessToken);
+  //     router.push('/onboarding');
+  //   } else {
+  //     router.push(API_BASE_URL + '/signup');
+  //   }
+  // } catch (error) {
+  //   console.log('토큰을 확인할 수 없습니다.', error);
+  // }
 }
 
 export async function GetKakaoAuth() {
-  const router = useRouter();
   try {
+    const router = useRouter();
     const response = await axiosInstance.get(`/auth/kakao`);
     if (response.data.registered === true) {
       const { accessToken } = response.data;
       saveTokenToCookie(accessToken);
       router.push('/onboarding');
     } else {
-      router.push('/signup');
+      router.push(API_BASE_URL + '/signup');
     }
   } catch (error) {
     console.log('토큰을 확인할 수 없습니다.', error);
--- src/components/auth/LoginButton/GoogleLogin.tsx
+++ src/components/auth/LoginButton/GoogleLogin.tsx
@@ -1,18 +1,43 @@
-import Link from 'next/link';
+'use client';
+
 import classNames from 'classnames/bind';
 import GoogleLogo from '@/assets/svgs/google-logo.svg';
+import { UseMutationOptions, useMutation } from '@tanstack/react-query';
+import { GetGoogleAuth, GoogleAuthResponse } from '@/apis/authAPI';
+import saveTokenToCookie from '@/utils/cookie';
+import { useRouter } from 'next/router';
+import { useEffect } from 'react';
 import styles from './Login.module.scss';
-import { API_BASE_URL } from '@/constants';
 
 const cx = classNames.bind(styles);
 
 export default function GoogleLogin() {
+  const router = useRouter();
+  const mutation = useMutation<GoogleAuthResponse, Error, void>({
+    mutationFn: GetGoogleAuth,
+    onSuccess: data => {
+      console.log(data);
+      if (data.registered === true) {
+        const { accessToken } = data;
+        saveTokenToCookie(accessToken as string);
+        router.push('/onboarding');
+      } else {
+        router.push('/signup');
+      }
+    },
+    onError: error => {
+      console.log('토큰을 확인할 수 없습니다.', error);
+    },
+  } as UseMutationOptions<GoogleAuthResponse, Error, void>);
+
+  // useEffect(() => {
+  //   mutation.mutate();
+  // }, [mutation]);
+
   return (
-    <Link href={API_BASE_URL + '/auth/google'}>
-      <div className={cx('googleButton')}>
-        <GoogleLogo />
-        <span>Google로 계속하기</span>
-      </div>
-    </Link>
+    <div className={cx('googleButton')} onClick={GoogleLogin}>
+      <GoogleLogo />
+      <span>Google로 계속하기</span>
+    </div>
   );
 }
