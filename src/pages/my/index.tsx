e393b39eb0e60c775b5f997759b8aa8f0167c894
Seoin <144193370+Seoin02@users.noreply.github.com>
2024-06-15 02:11:37 +0900

    feat: 로그인x my 페이지 완성

--- src/pages/my/index.tsx
+++ src/pages/my/index.tsx
@@ -3,10 +3,12 @@ import classNames from 'classnames/bind';
 import useAuth from '@/hooks/useAuth';
 import LoginButton from '@/components/auth/LoginButton';
 import ImageBox from '@/components/common/ImageBox';
-import Sample from '@/assets/exampleProductImg.jpg';
+import Pets from '@/assets/images/logout-pets.png';
+import Menu from '@/components/auth/Menu';
 
 import styles from './My.module.scss';
-import Menu from '@/components/auth/Menu';
+import FloatingBox from '@/components/common/Layout/Footer/FloatingBox';
+import NavBottom from '@/components/common/Nav/Bottom';
 
 const cx = classNames.bind(styles);
 
@@ -20,11 +22,14 @@ export default function MyPage() {
           <br />
           친구와 함께 할인받으세요
         </span>
-        <ImageBox size="myPageFirstPhoto" src={Sample} alt="강아지와 고양이가 환영해주는 사진" />
+        <ImageBox size="myPageFirstPhoto" src={Pets} alt="강아지와 고양이가 환영해주는 사진" />
         <LoginButton />
         <Link href="/">
           <div className={cx('notRegister')}>일단 둘러볼게요</div>
         </Link>
+        <FloatingBox>
+          <NavBottom />
+        </FloatingBox>
       </div>
     );
   if (isLogin) return <Menu />;
