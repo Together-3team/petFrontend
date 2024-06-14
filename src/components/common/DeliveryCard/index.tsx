8d93186ce70e10860846040ae0966a1db4fe8c9f
Songeun <hannahkeeien@gmail.com>
2024-06-14 17:18:04 +0900

    feat: deliveryCard 스타일, 엠티뷰 추가

--- src/components/common/DeliveryCard/index.tsx
+++ src/components/common/DeliveryCard/index.tsx
@@ -1,12 +1,42 @@
 import classNames from 'classnames/bind';
 import styles from './DeliveryCard.module.scss';
+import Button from '../Button';
+
+interface DeliveryInfo {
+  id: number;
+  name: string;
+  recipient: string;
+  recipientPhoneNumber: string;
+  zipCode: number;
+  address: string;
+  detailedAddress: string;
+  isDefault: boolean;
+}
 
 const cx = classNames.bind(styles);
 
-export default function DeliveryCard() {
+export default function DeliveryCard({ deliveryInfo }: { deliveryInfo: DeliveryInfo }) {
+  const { id, name, recipient, recipientPhoneNumber, zipCode, address, detailedAddress, isDefault } = deliveryInfo;
+
   return (
     <div>
-      <span></span>
+      <div className={cx('addressName')}>
+        <span>{name}</span>
+        {isDefault && <span>기본 배송지</span>}
+      </div>
+      <p className={cx('recipientInfo')}>
+        {recipient} ･ {recipientPhoneNumber}
+      </p>
+      <p>
+        {address}, {detailedAddress}
+      </p>
+      <p>{zipCode}</p>
+      <Button size="small" backgroundColor="$color-white-gray">
+        수정
+      </Button>
+      <Button size="small" backgroundColor="$color-white-gray">
+        삭제
+      </Button>
     </div>
   );
 }
--- src/pages/my/delivery/index.tsx
+++ src/pages/my/delivery/index.tsx
@@ -2,10 +2,47 @@ import classNames from 'classnames/bind';
 import BackButton from '@/components/common/Button/BackButton';
 import Header from '@/components/common/Layout/Header';
 import styles from './Delivery.module.scss';
+import DeliveryCard from '@/components/common/DeliveryCard';
+import { useEffect } from 'react';
+import DeliveryEmptyView from '@/components/delivery/EmptyView';
+
+interface DeliveryInfo {
+  id: number;
+  name: string;
+  recipient: string;
+  recipientPhoneNumber: string;
+  zipCode: number;
+  address: string;
+  detailedAddress: string;
+  isDefault: boolean;
+}
 
 const cx = classNames.bind(styles);
 
 export default function MyDeliveryPage() {
+  useEffect(() => {});
+  const deliverlies: DeliveryInfo[] = [
+    // {
+    //   id: 1,
+    //   name: '김견주 집',
+    //   recipient: '김견주',
+    //   recipientPhoneNumber: '010-1111-2222',
+    //   zipCode: 0o2233,
+    //   address: '서울 마포구 마포로 85 ',
+    //   detailedAddress: '102동 1012호',
+    //   isDefault: true,
+    // },
+    // {
+    //   id: 2,
+    //   name: '김견주 회사',
+    //   recipient: '김견주',
+    //   recipientPhoneNumber: '010-1111-3333',
+    //   zipCode: 12393,
+    //   address: '서울 마포구 마포로 85 ',
+    //   detailedAddress: '102동 1013호',
+    //   isDefault: true,
+    // },
+  ];
   return (
     <div style={{ margin: '20px 16px' }}>
       <Header.Root>
@@ -16,6 +53,13 @@ export default function MyDeliveryPage() {
           <h1 className={cx('title')}>배송지 목록</h1>
         </Header.Box>
       </Header.Root>
+      {deliverlies.length !== 0 ? (
+        deliverlies.map(deliveryInfo => {
+          return <DeliveryCard key={deliveryInfo.id} deliveryInfo={deliveryInfo} />;
+        })
+      ) : (
+        <DeliveryEmptyView />
+      )}
     </div>
   );
 }
