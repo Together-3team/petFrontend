fe0b191be9f0416722aaf76eee0bc1dca447c896
Seoin <144193370+Seoin02@users.noreply.github.com>
2024-06-15 02:44:57 +0900

    feat: onboarding 페이지 그래픽 이미지 적용

--- src/pages/onboarding/index.tsx
+++ src/pages/onboarding/index.tsx
@@ -1,14 +1,24 @@
 import Link from 'next/link';
+import { useState } from 'react';
 import { FieldValues, FormProvider, SubmitHandler, useForm } from 'react-hook-form';
 import ImageBox from '@/components/common/ImageBox';
 import Button from '@/components/common/Button';
-import Image from '@/assets/exampleProductImg.jpg';
+import selectedDog from '@/assets/images/selected-dog.png';
+import selectedCat from '@/assets/images/selected-cat.png';
+import unselectedCat from '@/assets/images/unselected-cat.png';
+import unselectedDog from '@/assets/images/unselected-dog.png';
+
 import styles from './Onboarding.module.scss';
 
 export default function Onboarding() {
+  const [isChecked, setIsChecked] = useState<string | null>();
   const methods = useForm();
   const { register, handleSubmit } = methods;
   const onSubmit: SubmitHandler<FieldValues> = data => console.log(data);
+
+  function handleCheckboxChange(key: string) {
+    setIsChecked(prev => (prev === key ? null : key));
+  }
 
   return (
     <FormProvider {...methods}>
@@ -21,9 +31,20 @@ export default function Onboarding() {
           </h1>
           <div className={styles.petChoice}>
             <div className={styles.petChoiceBox}>
-              <ImageBox size="petPhoto" src={Image} alt="강아지 이미지" />
+              <ImageBox
+                size="petPhoto"
+                src={isChecked === 'dog' ? selectedDog : unselectedDog}
+                alt={isChecked === 'cat' ? '선택된 강아지 이미지' : '미선택 강아지 이미지'}
+              />
               <label className={styles.petChoiceLabel}>
-                <input type="checkbox" className={styles.checkboxInput} {...register('dog')} />
+                <input
+                  key="dog"
+                  type="checkbox"
+                  className={styles.checkboxInput}
+                  checked={isChecked === 'dog'}
+                  onClick={() => handleCheckboxChange('dog')}
+                  {...register('dog')}
+                />
                 <div className={styles.petChoiceButton}>
                   <span className={styles.buttonText}>강아지</span>
                   <div className={styles.checkIcon} />
@@ -31,9 +52,20 @@ export default function Onboarding() {
               </label>
             </div>
             <div className={styles.petChoiceBox}>
-              <ImageBox size="petPhoto" src={Image} alt="고양이 이미지" />
+              <ImageBox
+                size="petPhoto"
+                src={isChecked === 'cat' ? selectedCat : unselectedCat}
+                alt={isChecked === 'cat' ? '선택된 고양이 이미지' : '미선택 고양이 이미지'}
+              />
               <label className={styles.petChoiceLabel}>
-                <input type="checkbox" className={styles.checkboxInput} {...register('cat')} />
+                <input
+                  key="cat"
+                  type="checkbox"
+                  className={styles.checkboxInput}
+                  checked={isChecked === 'cat'}
+                  onClick={() => handleCheckboxChange('cat')}
+                  {...register('cat')}
+                />
                 <div className={styles.petChoiceButton}>
                   <span className={styles.buttonText}>고양이</span>
                   <div className={styles.checkIcon} />
