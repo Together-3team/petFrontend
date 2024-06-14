215f1fc4370d4d51687a1455b13ff3f1b23446b5
Songeun <hannahkeeien@gmail.com>
2024-06-14 17:25:12 +0900

    feat: 배송지 추가 버튼

--- src/components/delivery/EmptyView/index.tsx
+++ src/components/delivery/EmptyView/index.tsx
@@ -4,5 +4,11 @@ import styles from './EmptyView.module.scss';
 const cx = classNames.bind(styles);
 
 export default function DeliveryEmptyView() {
-  return <div className={cx('deliveryEmptyView')}>아직 배송지가 등록되지 않았어요.</div>;
+  return (
+    <>
+      {' '}
+      <div className={cx('deliveryEmptyView')}>아직 배송지가 등록되지 않았어요.</div>
+      <button>배송지 추가 버튼</button>
+    </>
+  );
 }
--- src/pages/my/delivery/index.tsx
+++ src/pages/my/delivery/index.tsx
@@ -22,26 +22,26 @@ const cx = classNames.bind(styles);
 export default function MyDeliveryPage() {
   useEffect(() => {});
   const deliverlies: DeliveryInfo[] = [
-    // {
-    //   id: 1,
-    //   name: '김견주 집',
-    //   recipient: '김견주',
-    //   recipientPhoneNumber: '010-1111-2222',
-    //   zipCode: 0o2233,
-    //   address: '서울 마포구 마포로 85 ',
-    //   detailedAddress: '102동 1012호',
-    //   isDefault: true,
-    // },
-    // {
-    //   id: 2,
-    //   name: '김견주 회사',
-    //   recipient: '김견주',
-    //   recipientPhoneNumber: '010-1111-3333',
-    //   zipCode: 12393,
-    //   address: '서울 마포구 마포로 85 ',
-    //   detailedAddress: '102동 1013호',
-    //   isDefault: true,
-    // },
+    {
+      id: 1,
+      name: '김견주 집',
+      recipient: '김견주',
+      recipientPhoneNumber: '010-1111-2222',
+      zipCode: 0o2233,
+      address: '서울 마포구 마포로 85 ',
+      detailedAddress: '102동 1012호',
+      isDefault: true,
+    },
+    {
+      id: 2,
+      name: '김견주 회사',
+      recipient: '김견주',
+      recipientPhoneNumber: '010-1111-3333',
+      zipCode: 12393,
+      address: '서울 마포구 마포로 85 ',
+      detailedAddress: '102동 1013호',
+      isDefault: true,
+    },
   ];
   return (
     <div style={{ margin: '20px 16px' }}>
