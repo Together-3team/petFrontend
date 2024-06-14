74edde822dbec9dc13fc5ce8fd3bc320864d344d
Songeun <hannahkeeien@gmail.com>
2024-06-14 14:33:31 +0900

    feat: DeliveryCard 생성

---
+++ src/components/common/DeliveryCard/index.tsx
@@ -0,0 +1,12 @@
+import classNames from 'classnames/bind';
+import styles from './DeliveryCard.module.scss';
+
+const cx = classNames.bind(styles);
+
+export default function DeliveryCard() {
+  return (
+    <div>
+      <span></span>
+    </div>
+  );
+}
