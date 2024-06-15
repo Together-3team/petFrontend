4b59dfb71631ca18f65f9e387877277e81f77622
devwqc <devwqc@gmail.com>
2024-06-15 22:09:42 +0900

    comment: BottomModal 사용법 작성

--- src/components/common/Modal/Base/BottomModal/index.tsx
+++ src/components/common/Modal/Base/BottomModal/index.tsx
@@ -12,6 +12,25 @@ interface BottomModalProps extends PropsWithChildren {
   hasBackdrop?: boolean;
 }
 
+/*
+  바닥에 고정된 모달 형태의 베이스입니다.
+  페이지 컴포넌트에서 아래 예시처럼 사용하셔도 되고 이 컴포넌트를 사용해서 따로 컴포넌트를 만드셔도 됩니다.
+
+  사용⭐️)
+  export default function Example() {
+    const { modalOpen, handleModalOpen, handleModalClose } = useModal();
+
+    return (
+      <div>
+        <button type="button" onClick={handleModalOpen}>모달 오픈</div>
+
+        <BottomModal isOpen={modalOpen} onClose={handleModalClose}>
+          <div>모달 내용</div>
+        </BottomModal>
+      </div>
+    )
+  }
+*/
 export default function BottomModal({ id, isOpen, onClose, hasBackdrop = true, children }: BottomModalProps) {
   useEffect(() => {
     const toggleModal = () => {
