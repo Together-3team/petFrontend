5af945599c4506b61f1c2655cfaaac4141fcbcd2
devwqc <devwqc@gmail.com>
2024-06-15 22:11:28 +0900

    comment: CenterModal 사용법 작성

--- src/components/common/Modal/Base/CenterModal/index.tsx
+++ src/components/common/Modal/Base/CenterModal/index.tsx
@@ -13,6 +13,25 @@ interface CenterModalProps extends PropsWithChildren {
   hasBackdrop?: boolean;
 }
 
+/*
+  가운데 고정된 모달 형태의 베이스입니다.
+  페이지 컴포넌트에서 아래 예시처럼 사용하셔도 되고 이 컴포넌트를 사용해서 따로 컴포넌트를 만드셔도 됩니다.
+
+  사용⭐️)
+  export default function Example() {
+    const { modalOpen, handleModalOpen, handleModalClose } = useModal();
+
+    return (
+      <div>
+        <button type="button" onClick={handleModalOpen}>모달 오픈</div>
+
+        <CenterModal isOpen={modalOpen} onClose={handleModalClose}>
+          <div>모달 내용</div>
+        </CenterModal>
+      </div>
+    )
+  }
+*/
 export default function CenterModal({ id, isOpen, onClose, hasBackdrop = true, children }: CenterModalProps) {
   useEffect(() => {
     const toggleModal = () => {
