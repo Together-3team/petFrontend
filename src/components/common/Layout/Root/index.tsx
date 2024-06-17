0a917b621b8eb492c268ae3e75a61209022242e0
devwqc <devwqc@gmail.com>
2024-06-17 19:41:43 +0900

    feat: RootLayout에 IntroView 적용

--- src/components/common/Layout/Root/index.tsx
+++ src/components/common/Layout/Root/index.tsx
@@ -1,25 +1,14 @@
 import { PropsWithChildren } from 'react';
 
 import styles from './RootLayout.module.scss';
-import LogoIcon from '@/assets/svgs/heart.svg';
 import { PORTAL_ID } from '@/constants/portal';
+import IntroView from '../IntroView';
 export default function RootLayout({ children }: PropsWithChildren) {
   return (
     <div className={styles.container}>
       <div className={styles.homeBackground} />
-      <div className={styles.heroContainer}>
-        {/* hero 내용 분리 예정 */}
-        <div className={styles.hero}>
-          <header>
-            {/* 로고 이미지로 대체 예정 */}
-            <LogoIcon />
-            <p>반려동물 용품</p>
-            <p>공구로 더 저렴하게!</p>
-          </header>
-          <footer>
-            <button type="button">친구에게도 알려주기</button>
-          </footer>
-        </div>
+      <div className={styles.introViewBox}>
+        <IntroView />
       </div>
       <div className={styles.mainBox}>
         <div className={styles.main}>
