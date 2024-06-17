bb883a925d3d5166608f082e8eb9357676bccfdc
seolsis <seoljw1005@naver.com>
2024-06-17 17:38:51 +0900

    fix: lottie 파일 적용

--- src/pages/payment/paymentSuccess.tsx
+++ src/pages/payment/paymentSuccess.tsx
@@ -2,21 +2,30 @@ import { useEffect } from 'react';
 import { useRouter } from 'next/router';
 import styles from './PaymentSuccess.module.scss';
 import exampleProductImg from '@/assets/exampleProductImg.jpg';
+import Lottie from 'react-lottie-player';
+import check from '@/assets/images/check.json';
 import Image from 'next/image';
 
 export default function PaymentSuccess() {
   const router = useRouter();
 
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      router.push('/payment/paymentSuccessByCart');
-    }, 2000);
+  // useEffect(() => {
+  //   const timer = setTimeout(() => {
+  //     router.push('/payment/paymentSuccessByCart');
+  //   }, 2000);
 
-    return () => clearTimeout(timer);
-  }, [router]);
+  //   return () => clearTimeout(timer);
+  // }, [router]);
   return (
     <div className={styles.paymentSuccess}>
-      <Image className={styles.successImg} width={180} height={180} src={exampleProductImg} alt="successImg" />
+      {/* <Image className={styles.successImg} width={180} height={180} src={exampleProductImg} alt="successImg" /> */}
+      <Lottie
+        className={styles.successImg}
+        loop={false}
+        animationData={check}
+        play
+        style={{ width: 180, height: 180 }}
+      />
       <div className={styles.finish}>결제 완료!</div>
     </div>
   );
