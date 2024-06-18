10ead368ac97fa234c6e03f572f67ed37997d7a6
devwqc <devwqc@gmail.com>
2024-06-18 15:30:06 +0900

    style: NotFound . -> , 변경

--- src/components/common/NotFound/index.tsx
+++ src/components/common/NotFound/index.tsx
@@ -9,7 +9,7 @@ export default function NotFound() {
       <WarningPawingIcon />
       <p className={styles.title}>페이지를 찾을 수 없습니다.</p>
       <div className={styles.description}>
-        <p>페이지의 주소가 잘못 입력되었거나.</p>
+        <p>페이지의 주소가 잘못 입력되었거나,</p>
         <p>주소가 변경 혹은 삭제되어 사용하실 수 없습니다.</p>
       </div>
       <Link href="/" className={styles.link}>
