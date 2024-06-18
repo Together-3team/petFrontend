372921622bc8bfa3549535c07db5830e983bb99b
Seoin <144193370+Seoin02@users.noreply.github.com>
2024-06-18 11:52:01 +0900

    refactor: GetServerSideProps 추가

--- src/pages/onboarding/welcome.tsx
+++ src/pages/onboarding/welcome.tsx
@@ -2,6 +2,10 @@ import { useRouter } from 'next/router';
 import useAuth from '@/hooks/useAuth';
 import ImageBox from '@/components/common/ImageBox';
 import Image from '@/assets/exampleProductImg.jpg';
+import { fetchMyData } from '@/apis/userApi';
+import { QueryClient, dehydrate } from '@tanstack/react-query';
+import { GetServerSidePropsContext } from 'next';
+
 import styles from './Welcome.module.scss';
 
 export default function Welcome() {
@@ -20,4 +24,18 @@ export default function Welcome() {
       <ImageBox size="welcomePetPhoto" src={Image} alt="환영 이미지" />
     </div>
   );
+}
+
+export async function getServerSideProps(context: GetServerSidePropsContext) {
+  const queryClient = new QueryClient();
+
+  const accessToken = context.req.cookies['accessToken'];
+
+  await queryClient.prefetchQuery({ queryKey: ['user', accessToken], queryFn: fetchMyData });
+
+  return {
+    props: {
+      dehydratedState: dehydrate(queryClient),
+    },
+  };
 }
