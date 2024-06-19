ae1085c347cae748bf60c82886f95be91596222b
minyoung <alsdyd1021@naver.com>
2024-06-19 11:03:00 +0900

    feat: product option API 연결 및 bottomSheet 환경 세팅

---
+++ src/components/product/OptionBottomSheet.tsx
@@ -0,0 +1,7 @@
+import useModal from '@/hooks/useModal';
+
+export default function OptionBottomSheet() {
+  const { modalOpen, handleModalOpen, handleModalClose } = useModal();
+
+  return <div>OptionBottomSheet</div>;
+}
