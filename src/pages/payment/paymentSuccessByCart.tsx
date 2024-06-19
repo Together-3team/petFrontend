d9aa52463ccb01702926f745804c33e9b8caf979
seolsis <seoljw1005@naver.com>
2024-06-19 11:47:35 +0900

    feat: x버튼

--- src/pages/payment/paymentSuccessByCart.tsx
+++ src/pages/payment/paymentSuccessByCart.tsx
@@ -4,6 +4,9 @@ import styles from './PaymentSuccessByCart.module.scss';
 import paymentSuccess from '@/assets/images/paymentSuccess.png';
 import Image from 'next/image';
 import { completePayment } from '@/apis/paymentApi';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faXmark } from '@fortawesome/free-solid-svg-icons';
+
 export default function PaymentSuccessByCart() {
   const [buttonText, setButtonText] = useState('링크 복사하기');
   const [buttonColor, setButtonColor] = useState(styles.copyButton);
@@ -27,19 +30,22 @@ export default function PaymentSuccessByCart() {
   }
 
   return (
-    <div className={styles.paymentSuccessByCart}>
-      <div className={styles.warning}>
-        24시간 내 성사되지 않으면
-        <br />
-        주문이 취소될 수 있어요!
+    <>
+      <FontAwesomeIcon icon={faXmark} className={styles.faXmark} onClick={() => router.push('/')} />
+      <div className={styles.paymentSuccessByCart}>
+        <div className={styles.warning}>
+          24시간 내 성사되지 않으면
+          <br />
+          주문이 취소될 수 있어요!
+        </div>
+        <Image className={styles.petImg} width={180} height={180} src={paymentSuccess} alt="petImg" />
+        <div className={styles.share}>
+          <div className={styles.shareMent}>공유하고 친구와 같이 할인받기</div>
+          <button className={buttonColor} onClick={handleCopyLink}>
+            {buttonText}
+          </button>
+        </div>
       </div>
-      <Image className={styles.petImg} width={180} height={180} src={paymentSuccess} alt="petImg" />
-      <div className={styles.share}>
-        <div className={styles.shareMent}>공유하고 친구와 같이 할인받기</div>
-        <button className={buttonColor} onClick={handleCopyLink}>
-          {buttonText}
-        </button>
-      </div>
-    </div>
+    </>
   );
 }
