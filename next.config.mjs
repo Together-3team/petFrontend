1df5d7add8b1b57197c48eed80499f912cbd8e6e
devwqc <devwqc@gmail.com>
2024-06-22 12:34:06 +0900

    Revert "chore: next.config.mjs images remotePatterns hostname 전부 허용"
    This reverts commit e97b4c94e41bceac3f043d66c396c3ee1abad8a5.

--- next.config.mjs
+++ next.config.mjs
@@ -9,11 +9,6 @@ const nextConfig = {
       'lh3.googleusercontent.com',
       'review-image-3team.s3.ap-northeast-2.amazonaws.com',
     ],
-    remotePatterns: [
-      {
-        hostname: '**',
-      },
-    ],
   },
   sassOptions: {
     includePaths: ['styles'],
