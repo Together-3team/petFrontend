5dc38fc353426129cef3337ffa3e0383d0ce773b
Songeun <hannahkeeien@gmail.com>
2024-06-22 20:34:17 +0900

    refactor: 이전 커밋 합치기

--- src/pages/products/[id]/index.tsx
+++ src/pages/products/[id]/index.tsx
@@ -14,6 +14,12 @@ import { Product } from '@/types/product';
 import styles from './ProductDetail.module.scss';
 import HighlightTeam from '@/components/common/Team/HighlightTeam';
 import HighlightReview from '@/components/common/review/HighlightReview';
+import OptionBottomSheet from '@/components/product/OptionBottomSheet';
+import useModal from '@/hooks/useModal';
+import Zzim from '@/components/common/Zzim';
+import Button from '@/components/common/Button';
+import BottomModal from '@/components/common/Modal/Base/BottomModal';
+import useAuth from '@/hooks/useAuth';
 
 const cx = classNames.bind(styles);
 
@@ -37,9 +43,21 @@ export async function getServerSideProps(context: GetServerSidePropsContext) {
 }
 
 export default function ProductDetailPage({ product }: { product: Product }) {
+  const { modalOpen, handleModalOpen, handleModalClose } = useModal();
+  const {
+    modalOpen: secondModalOpen,
+    handleModalOpen: handleSecondModalOpen,
+    handleModalClose: handleSecondModalClose,
+  } = useModal();
   const router = useRouter();
   const { id } = router.query;
   const productId = Number(id);
+  const { isLogin } = useAuth();
+
+  const handleLoginButtonClick = () => {
+    router.replace({ pathname: '/my', query: { prevPath: router.asPath } });
+  };
+
   return (
     <>
       <Header.Root className={cx('headerRoot')}>
@@ -63,6 +81,38 @@ export default function ProductDetailPage({ product }: { product: Product }) {
         </div>
         <OrderPolicy productId={productId} />
       </div>
+      <div className={cx('fixedCta')}>
+        <div className={cx('zzim')}>
+          <Zzim color="gray" productId={productId} />
+        </div>
+        <div className={cx('button')}>
+          <Button
+            size="large"
+            backgroundColor="$color-pink-main"
+            onClick={isLogin ? handleModalOpen : handleSecondModalOpen}>
+            구매하기
+          </Button>
+        </div>
+      </div>
+      {/* <OptionBottomSheet isOpen={modalOpen} onClose={handleModalClose} productId={productId} type="purchaseOnly" /> */}
+      <OptionBottomSheet isOpen={modalOpen} onClose={handleModalClose} productId={productId} type="cartPurchase" />
+      <BottomModal isOpen={secondModalOpen} onClose={handleSecondModalClose}>
+        <div className={cx('modalContents')}>
+          <p className={cx('modalTitle')}>
+            로그인이 필요합니다.
+            <br />
+            로그인 페이지로 이동하시겠습니까?
+          </p>
+          <div className={cx('modalButtons')}>
+            <Button size="medium" backgroundColor="$color-white" onClick={handleSecondModalClose}>
+              취소
+            </Button>
+            <Button size="medium" backgroundColor="$color-gray-800" onClick={handleLoginButtonClick}>
+              확인
+            </Button>
+          </div>
+        </div>
+      </BottomModal>
     </>
   );
 }
