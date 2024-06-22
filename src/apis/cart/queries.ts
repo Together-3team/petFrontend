5c1cbc9e7ab53a3ddd04124c2ca5c202bbea185b
devwqc <devwqc@gmail.com>
2024-06-22 16:06:02 +0900

    feat: cartQueries 구현

---
+++ src/apis/cart/queries.ts
@@ -0,0 +1,24 @@
+import { queryOptions } from '@tanstack/react-query';
+
+import { fetchCartProducts } from '@/apis/cartApi';
+import { queryClient } from '@/utils/queryClient';
+
+export const keys = {
+  cart: () => ['cart'],
+};
+
+export const cartQueries = {
+  queryKey: () => keys.cart(),
+  queryOptions: () => {
+    return queryOptions({
+      queryKey: cartQueries.queryKey(),
+      queryFn: () => fetchCartProducts(),
+    });
+  },
+  prefetchQuery: () => {
+    queryClient.prefetchQuery(cartQueries.queryOptions());
+  },
+  removeQueries: () => {
+    queryClient.removeQueries({ queryKey: cartQueries.queryKey() });
+  },
+};
