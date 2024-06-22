3425e8d0cb6f81873f69a02d9fc0a521d68221b7
minyoung <alsdyd1021@naver.com>
2024-06-22 23:59:08 +0900

    refactor: HighlightReview data 개수 수정

--- src/components/common/review/HighlightReview.tsx
+++ src/components/common/review/HighlightReview.tsx
@@ -1,21 +1,22 @@
+import { useEffect, useState } from 'react';
 import Link from 'next/link';
 import RatingBox from './RatingBox';
 import ReviewBox from './ReviewBox';
-import styles from './HighlightReview.module.scss';
-import { useEffect, useState } from 'react';
 import { httpClient } from '@/apis/httpClient';
 import { Product, ProductReview } from '@/types/product';
+import styles from './HighlightReview.module.scss';
 
 export default function HighlightReview({ productId }: any) {
   const [reviewData, setReviewData] = useState<ProductReview[]>([]);
   const [averageRating, setAverageRating] = useState<string>('');
+  const [reviewCount, setReviewCount] = useState<number>(0);
 
   useEffect(() => {
     const fetchReviewData = async () => {
       try {
         const response = await httpClient().get<Product>(`products/detail/${productId}`);
-        console.log(response);
-        setReviewData(response.reviews);
+        setReviewData(response.reviews.slice(0, 3));
+        setReviewCount(response.reviews.length);
         setAverageRating(response.averageRating.toFixed(1));
       } catch (error) {
         console.log(error);
@@ -31,9 +32,9 @@ export default function HighlightReview({ productId }: any) {
         <>
           <div className={styles.sectionTitleBox}>
             <p className={styles.sectionTitle}>리뷰</p>
-            <p className={styles.totalReview}>{reviewData.length}</p>
+            <p className={styles.totalReview}>{reviewCount}</p>
           </div>
-          <RatingBox rating={averageRating} totalReviewer={reviewData.length} className={styles.ratingBoxStyle} />
+          <RatingBox rating={averageRating} totalReviewer={reviewCount} className={styles.ratingBoxStyle} />
           <div className={styles.reviewContainer}>
             {reviewData.map(data => (
               <ReviewBox key={data.id} reviewData={data} className={styles.reviewBoxStyle} />
