566dfa832f798309c85e51f38a8bdc50fa11b8e6
Seoin <144193370+Seoin02@users.noreply.github.com>
2024-06-22 10:52:53 +0900

    fix: 빌드 에러 수정

--- src/pages/my/order/detail.tsx
+++ src/pages/my/order/detail.tsx
@@ -107,7 +107,6 @@ export default function OrderDetail() {
       <div className={styles.orderDetailBottom}>
         <div className={styles.rectangle} />
         <div className={styles.orderPriceArea}>
-          <TotalPay title={`결제 금액`} totalPrice={totalPrice} totalOriginalPrice={totalOriginalPrice} productCount={orderCount} />
         </div>
         <div className={styles.paymentMethod}>
           <div className={styles.rectangle} />
