041760d0586db025958b49de05fafaf43da67655
devwqc <devwqc@gmail.com>
2024-06-23 04:06:31 +0900

    feat: liked 페이지 구현

--- src/pages/liked/index.tsx
+++ src/pages/liked/index.tsx
@@ -1,92 +1,67 @@
-import { useRouter } from 'next/router';
+import { GetServerSidePropsContext } from 'next';
+import { dehydrate } from '@tanstack/react-query';
 
 import styles from './LikedPage.module.scss';
 import Header from '@/components/common/Layout/Header';
-import LogoFull from '@/components/common/Icon/LogoFull';
-import SearchButton from '@/components/common/Button/Search';
 import CartButton from '@/components/common/Button/Cart';
-import NavTop from '@/components/common/Nav/Top';
 import GitHubBox from '@/components/common/GitHubBox';
 import FloatingBox from '@/components/common/Layout/Footer/FloatingBox';
 import NavBottom from '@/components/common/Nav/Bottom';
-import SortButton from '@/components/common/Button/Sort';
 import useToast from '@/hooks/useToast';
-import PetToggleButton from '@/components/common/Button/PetToggle';
 import ScrollTopButton from '@/components/common/Button/ScrollTop';
-import CardListHot from '@/components/common/Card/CardList/Hot';
-import CardListBasic from '@/components/common/Card/CardList/Basic';
-
-const SORT_OPTIONS = [
-  { name: '최신순', value: '0' },
-  { name: '별점 높은 순', value: '1' },
-  { name: '별점 낮은 순', value: '2' },
-  { name: '가격 높은 순', value: '3' },
-  { name: '가격 낮은 순', value: '4' },
-];
+import CardListZzim from '@/components/common/Card/CardList/Zzim';
+import { httpClient } from '@/apis/httpClient';
+import { UserResponse } from '@/apis/userApi';
+import { queryClient } from '@/utils/queryClient';
+import { zzimsQueries } from '@/apis/product/queries';
 
 const BOTTOM_BOX_ID = 'bottomBox';
 
-export async function getServerSideProps() {
+export async function getServerSideProps(context: GetServerSidePropsContext) {
+  const accessToken = context.req.cookies['accessToken'];
+
+  await zzimsQueries.prefetchQuery();
+
+  try {
+    await httpClient().get<UserResponse>(`/users/me`, { headers: { Authorization: `Bearer ${accessToken}` } });
+  } catch {
+    return {
+      redirect: {
+        destination: '/my',
+        permanent: false,
+      },
+    };
+  }
+
   return {
-    props: {},
+    props: {
+      dehydratedState: dehydrate(queryClient),
+    },
   };
 }
 
-interface LikedPageProps {
-  petType: string;
-  orderBy: string;
-}
-
-export default function LikedPage({ petType, orderBy }: LikedPageProps) {
-  const router = useRouter();
+export default function LikedPage() {
   const { showToast, setPortalId } = useToast(BOTTOM_BOX_ID);
 
   return (
     <div className={styles.layout}>
       <Header.Root className={styles.header}>
         <Header.Box className={styles.headerBox}>
-          <Header.Left>찜</Header.Left>
+          <Header.Left className={styles.title}>찜</Header.Left>
           <Header.Right>
             <CartButton />
           </Header.Right>
         </Header.Box>
       </Header.Root>
-      <div className={styles.sortBox}>
-        <SortButton
-          options={SORT_OPTIONS}
-          initialOptionValue={orderBy}
-          onClick={value => {
-            router.replace({
-              pathname: '/products/hot',
-              query: {
-                ...router.query,
-                orderBy: value,
-              },
-            });
-          }}
-        />
-      </div>
       <div className={styles.divider} />
       <div className={styles.contents}>
-        <CardListBasic />
+        <CardListZzim />
       </div>
       <GitHubBox />
       <FloatingBox id={BOTTOM_BOX_ID}>
         <NavBottom />
         <div className={styles.floatingActionBox}>
           <ScrollTopButton />
-          <PetToggleButton
-            initialPetType={petType}
-            onClick={petType => {
-              router.replace({
-                pathname: '/products/hot',
-                query: {
-                  ...router.query,
-                  petType: petType.value,
-                },
-              });
-            }}
-          />
         </div>
       </FloatingBox>
     </div>
