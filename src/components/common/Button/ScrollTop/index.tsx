0210ca58a0aa468cd981387de4e679cce6af394d
devwqc <devwqc@gmail.com>
2024-06-22 20:28:02 +0900

    feat: ScrollTopButton 구현

--- src/components/common/Button/ScrollTop/index.tsx
+++ src/components/common/Button/ScrollTop/index.tsx
@@ -1,18 +1,32 @@
-import { ButtonHTMLAttributes, PropsWithChildren } from 'react';
 import classNames from 'classnames/bind';
 
 import styles from './ScrollTopButton.module.scss';
+import ArrowTop from '@/assets/svgs/arrow-top-white.svg';
+import useScrollUpAndDown from '@/hooks/useScrollUpAndDown';
 
 const cx = classNames.bind(styles);
 
-interface ScrollTopButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+interface ScrollTopButtonProps {
   className?: string;
 }
 
-export default function ScrollTopButton({ className, children, ...rest }: PropsWithChildren<ScrollTopButtonProps>) {
+export default function ScrollTopButton({ className }: ScrollTopButtonProps) {
+  const { isUp } = useScrollUpAndDown(200);
+
+  const handleClick = () => {
+    window.scroll({
+      top: 0,
+      behavior: 'smooth',
+    });
+  };
+
   return (
-    <button type="button" className={cx('button', className)} {...rest}>
-      {children}
-    </button>
+    <>
+      {!isUp && (
+        <button type="button" className={cx('button', className)} onClick={handleClick}>
+          <ArrowTop />
+        </button>
+      )}
+    </>
   );
 }
