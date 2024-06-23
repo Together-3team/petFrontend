699347358bde653139e1e823714481f61c8ac366
devwqc <devwqc@gmail.com>
2024-06-23 18:19:39 +0900

    refactor: SearchKeywords 최근 검색어 선택 시 localhost 저장 추가

--- src/components/search/SearchKeywords/index.tsx
+++ src/components/search/SearchKeywords/index.tsx
@@ -2,6 +2,8 @@ import styles from './SearchKeywords.module.scss';
 import { Keyword } from '@/types/components/search.types';
 import CancelIcon from '@/assets/svgs/ic-x.svg';
 import Link from 'next/link';
+import { useRouter } from 'next/router';
+import { useEffect } from 'react';
 
 interface SearchKeywordsProps {
   keywords: Keyword[];
@@ -10,22 +12,39 @@ interface SearchKeywordsProps {
 
 export default function SearchKeywords({ keywords = [], onRemove }: SearchKeywordsProps) {
   const maxLength = keywords.length > 10 ? 10 : keywords.length;
+  const router = useRouter();
+
+  const handleSearch = (keyword: Keyword) => {
+    const newKeyword = { ...keyword };
+    const storedKeywordsJSON = localStorage.getItem('keywords') || '[]';
+    const storedKeywords: Keyword[] = JSON.parse(storedKeywordsJSON);
+
+    const oldKeywordIndex = storedKeywords.findIndex(storedKeyword => storedKeyword.text === newKeyword.text);
+
+    if (oldKeywordIndex > -1) {
+      storedKeywords.splice(oldKeywordIndex, 1);
+    }
+
+    const newKeywords = [newKeyword, ...storedKeywords];
+
+    localStorage.setItem('keywords', JSON.stringify(newKeywords));
+
+    router.replace({
+      pathname: '/search/result',
+      query: {
+        keyword: newKeyword.text,
+      },
+    });
+  };
 
   return (
     <ul className={styles.list}>
       {keywords.slice(0, maxLength).map((keyword, index) => (
         <>
           <li key={keyword.id} className={styles.item}>
-            <Link
-              href={{
-                pathname: '/search/result',
-                query: {
-                  keyword: keyword.text,
-                },
-              }}
-              className={styles.link}>
+            <button type="button" className={styles.keyword} onClick={() => handleSearch(keyword)}>
               {keyword.text}
-            </Link>
+            </button>
             <button type="button" className={styles.cancelButton} onClick={() => onRemove(keyword.id)}>
               <CancelIcon />
             </button>
