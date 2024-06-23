be293e1289cf7919a873bd4dc68bc40745a345d0
devwqc <devwqc@gmail.com>
2024-06-23 13:12:33 +0900

    feat: SearchKeywords 구분선 추가

--- src/components/search/SearchKeywords/index.tsx
+++ src/components/search/SearchKeywords/index.tsx
@@ -13,22 +13,25 @@ export default function SearchKeywords({ keywords = [], onRemove }: SearchKeywor
 
   return (
     <ul className={styles.list}>
-      {keywords.slice(0, maxLength).map(keyword => (
-        <li key={keyword.id} className={styles.item}>
-          <Link
-            href={{
-              pathname: '/search/result',
-              query: {
-                keyword: keyword.text,
-              },
-            }}
-            className={styles.link}>
-            {keyword.text}
-          </Link>
-          <button type="button" className={styles.cancelButton} onClick={() => onRemove(keyword.id)}>
-            <CancelIcon />
-          </button>
-        </li>
+      {keywords.slice(0, maxLength).map((keyword, index) => (
+        <>
+          <li key={keyword.id} className={styles.item}>
+            <Link
+              href={{
+                pathname: '/search/result',
+                query: {
+                  keyword: keyword.text,
+                },
+              }}
+              className={styles.link}>
+              {keyword.text}
+            </Link>
+            <button type="button" className={styles.cancelButton} onClick={() => onRemove(keyword.id)}>
+              <CancelIcon />
+            </button>
+          </li>
+          {index < maxLength - 1 && <div className={styles.divider} />}
+        </>
       ))}
     </ul>
   );
