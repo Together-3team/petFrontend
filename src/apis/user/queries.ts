5b920cef31c1198a3bc72eb76ae01b9105cd31ed
Seoin <144193370+Seoin02@users.noreply.github.com>
2024-07-23 17:50:35 +0900

    feat: userApi.put 쿼리키 정리

--- src/apis/user/queries.ts
+++ src/apis/user/queries.ts
@@ -1,19 +1,33 @@
-import { queryOptions, QueryClient } from '@tanstack/react-query';
+import { queryOptions, QueryClient, useQueryClient, useMutation } from '@tanstack/react-query';
 import { UserId, userApi } from './api';
 
-const key = {
+const keys = {
   user: () => ['user'],
 };
 
 const queryClient = new QueryClient();
 
 export const userQueries = {
-  getQueryKey: key.user,
+  getQueryKey: keys.user,
   removeQuery: () => queryClient.removeQueries({ queryKey: userQueries.getQueryKey() }),
   queryOptions: (id: UserId) => {
     return queryOptions({
       queryKey: [userQueries.getQueryKey(), id],
       queryFn: () => userApi.getUserData(id),
+    });
+  },
+  prefetchQuery: (id: UserId) => {
+    queryClient.prefetchQuery(userQueries.queryOptions(id));
+  },
+
+  useEditUserData: (id: UserId) => {
+    const queryClient = useQueryClient();
+    return useMutation({
+      mutationFn: userData => userApi.put(id, userData),
+      onSuccess: () => {
+        queryClient.invalidateQueries({ queryKey: userQueries.getQueryKey() });
+        queryClient.invalidateQueries({ queryKey: keys.user() });
+      },
     });
   },
 };
